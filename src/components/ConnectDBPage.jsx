0e164f07cb2f6110d30d84b11b3c19d7817bf717
tracer12 <vendetta12@naver.com>
2024-05-16 00:10:01 +0900

    최근 접속 시도한 url 표시

--- src/components/ConnectDBPage.jsx
+++ src/components/ConnectDBPage.jsx
@@ -1,20 +1,40 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 import useDatabaseStore from "../store/databaseStore";
 
 const ConnectDBPage = () => {
     const navigate = useNavigate();
-    const [value, setValue] = useState("");
     const [loading, setLoading] = useState(false);
     const { setDatabase } = useDatabaseStore();
+    const [recentlyUrlOpen, setRecentlyUrlOpen] = useState(false);
+    const [savedData, setSavedData] = useState({});
+    const [inputValue, setInputValue] = useState('');
+
+    const loadDataFromLocalStorage = () => {
+        const savedData = {};
+        for (let i = 0; i < localStorage.length; i++) {
+            const key = localStorage.key(i);
+            if (key.includes("mongo-es")) {
+                savedData[key] = JSON.parse(localStorage.getItem(key));
+            }
+        }
+        return savedData;
+    };
+
+    useEffect(() => {
+        const data = loadDataFromLocalStorage();
+        if (Object.keys(data).length > 0) {
+            setSavedData(data);
+        }
+    }, []);
 
     const connectCollection = async (e) => {
         e.preventDefault();
         setLoading(true);
         try {
-            // const response = await fetch(`${value}`, {
-            const response = await fetch('http://ec2-13-125-76-129.ap-northeast-2.compute.amazonaws.com:3000/api/v1/db/connect-mongo', {
+            const response = await fetch(`${inputValue}`, {
+                // const response = await fetch('http://ec2-13-125-76-129.ap-northeast-2.compute.amazonaws.com:3000/api/v1/db/connect-mongo', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -35,6 +55,21 @@ const ConnectDBPage = () => {
         }
     };
 
+    const recentlyUrlToggle = () => {
+        setRecentlyUrlOpen((prev) => (!prev));
+    }
+
+    const saveUrlLocal = (value) => {
+        const key = value + "-mongo-es"
+        localStorage.setItem(key, JSON.stringify(value));
+    };
+    const hasSavedUrl = Object.keys(savedData).length > 0;
+
+    const handleValueClick = (value) => {
+        setInputValue(value);
+    };
+
+
     return (
         <div className="position: absolute top-[250px] left-[300px]">
             <div className="text-lg">
@@ -43,11 +78,36 @@ const ConnectDBPage = () => {
             <div className="flex">
                 <input placeholder="url을 입력해주세요"
                     type="text" className="border border-1 border-slate-950 w-96"
-                    onChange={(e) => { setValue(e.target.value) }} />
-                <button className="border border-1 rounded-md border-slate-950" onClick={(e) => connectCollection(e)} disabled={loading}>
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)} />
+
+                <button className="border border-1 rounded-md border-slate-950" onClick={(e) => { connectCollection(e); saveUrlLocal(inputValue) }} disabled={loading}>
                     {loading ? 'Connecting...' : 'Connect'}
                 </button>
             </div>
+
+            <div className="">
+                <button
+                    className="border border-1 border-slate-950"
+                    onClick={recentlyUrlToggle}
+                    disabled={!hasSavedUrl}
+                >
+                    recently url
+                </button>
+            </div>
+            {recentlyUrlOpen && hasSavedUrl && (
+                <div className="">
+                    <ul className="border border-1 border-slate-950">
+                        {Object.entries(savedData).map(([key, value]) => (
+                            <li key={key} className="hover:bg-gray-400">
+                                <a onClick={() => { handleValueClick(value); recentlyUrlToggle() }}>
+                                    {value}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
         </div>
     )
 }
--- src/components/Home/ConfigurationScheme.jsx
+++ src/components/Home/ConfigurationScheme.jsx
@@ -26,9 +26,6 @@ const ConfigurationSchemeForm = () => {
                 })
             }).then(response => response.json())
                 .then(response => console.log(response));
-            // const data = await response.json();
-            // console.log(data);
-
         } catch (error) {
             console.error('Error:', error);
 
